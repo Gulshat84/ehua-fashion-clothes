810d072902fa6e3bd0034ac0e8738c9990651d64
Gulshat84 <gulshatik.nugumanova84@gmail.com>
2020-12-17 21:13:58 +0300

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -9,4 +9,27 @@ $(document).ready(function () {
     $(activeContent).addClass("trends-content__item--active");
     $(this).addClass("trends-tabs__item--active");
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  
+})
 });
